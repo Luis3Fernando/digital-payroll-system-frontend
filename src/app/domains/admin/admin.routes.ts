89215676a8c1f3bbf13c5d6081e23c73140d38d9
Luis3Fernando <luisfernando3chr@gmail.com>
2025-10-25 22:11:57 -0500

    Feat: Creación de pagina de auditoria

--- src/app/domains/admin/admin.routes.ts
+++ src/app/domains/admin/admin.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { AdminDashboardPage } from './components/admin-dashboard-page/admin-dashboard-page';
 import { UserManagementPage } from './components/user-management-page/user-management-page';
 import { BoletaManagementPage } from './components/boleta-management-page/boleta-management-page';
-import { SystemSettingsPage } from './components/system-settings-page/system-settings-page';
 import { LoginAdminPage } from './components/login-admin-page/login-admin-page';
 import { AdminLayout } from '../../core/layouts/admin-layout/admin-layout';
+import { AuditPage } from './components/audit-page/audit-page';
 
 export const adminRoutes: Routes = [
   { path: 'login', component: LoginAdminPage },
@@ -16,7 +16,7 @@ export const adminRoutes: Routes = [
       { path: 'dashboard', component: AdminDashboardPage },
       { path: 'usuarios', component: UserManagementPage },
       { path: 'boletas', component: BoletaManagementPage },
-      { path: 'configuracion', component: SystemSettingsPage },
+      { path: 'auditoria', component: AuditPage },
     ],
   },
 ];
---
+++ src/app/domains/admin/components/audit-page/audit-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-page',
+  imports: [],
+  templateUrl: './audit-page.html',
+  styles: ``
+})
+export class AuditPage {
+
+}
